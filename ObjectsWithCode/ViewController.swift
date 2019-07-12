1f2f6730cdaaa74b465e1b11ff8e452b35536cad
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-07-12 23:52:53 +0300

    first commit

--- ObjectsWithCode/ViewController.swift
+++ ObjectsWithCode/ViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var myLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        
+        myLabel.text = "Test Label"
+        myLabel.textAlignment = .center
+        myLabel.frame = CGRect(x: width * 0.5 - width * 0.8 / 2, y: height * 0.5 - 50/2, width: width * 0.8, height: 50)
+        view.addSubview(myLabel)
+        
+        let myButton = UIButton()
+        myButton.setTitle("My Button", for: UIControl.State.normal)
+        myButton.setTitleColor(UIColor.blue, for: UIControl.State.normal)
+        myButton.frame = CGRect(x: width * 0.5 - 100, y: height * 0.6, width: 200, height: 100)
+        view.addSubview(myButton)
+        
+        myButton.addTarget(self, action: #selector(ViewController.myAction), for: UIControl.Event.touchUpInside)
+        
+    }
+    
+    @objc func myAction() {
+        myLabel.text = "Tapped"
     }
 
 
